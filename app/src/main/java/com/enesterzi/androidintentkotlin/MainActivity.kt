9531ab9934a8870a1fbcd75a041b7ff7d771cf3b
Enes Terzi <enestrz15@gmail.com>
2024-04-16 20:38:10 +0300

    Initial commit

---
+++ app/src/main/java/com/enesterzi/androidintentkotlin/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.enesterzi.androidintentkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.enesterzi.androidintentkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.editText.setText("")
+
+    }
+
+    fun next(view: View) {
+        val intent = Intent(this@MainActivity, SecondActivity::class.java)
+        intent.putExtra("name", binding.editText.text.toString())
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/enesterzi/androidintentkotlin/SecondActivity.kt
@@ -0,0 +1,39 @@
+package com.enesterzi.androidintentkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.enesterzi.androidintentkotlin.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intentFromMain = intent //getIntent()
+        val name = intentFromMain.getStringExtra("name")
+        binding.nameText.text = name
+
+    }
+
+    fun previous(view: View) {
+        val intent = Intent(this@SecondActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
